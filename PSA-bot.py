1d4154b858f0abec1b29625162f55c30157394af
massi <maximiliano.ponce@usm.cl>
2023-02-19 03:14:26 -0300

    add buttons (bugs)

--- PSA-bot.py
+++ PSA-bot.py
@@ -1,5 +1,6 @@
 import discord
 from tokens import token_bot
+from discord.ui import Button, View
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -28,15 +29,42 @@ async def sens(ctx, base: float, member: discord.Member = None):
     name = member.display_name
     icon = member.display_avatar
 
+    async def lower_callback(interaction):
+        nonlocal message, name, icon, iterator, base, lower, higher, porc_low, porc_high
+        iterator += 1
+        porc_low += 0.1
+        porc_high -= 0.1
+        new_base = round((base + lower)/2,2)
+        new_lower = round(new_base * porc_low,2)
+        new_higher = round(new_base * porc_high,2)
+        await message.edit(embed=MyEmbed(name, icon, iterator, new_base, new_lower, new_higher), view=view)
+
+    async def higher_callback(interaction):
+        nonlocal message, name, icon, iterator, base, lower, higher, porc_low, porc_high
+        iterator += 1
+        porc_low += 0.1
+        porc_high -= 0.1
+        new_base = round((base + higher)/2,2)
+        new_lower = round(new_base * porc_low,2)
+        new_higher = round(new_base * porc_high,2)
+        await message.edit(embed=MyEmbed(name, icon, iterator, new_base, new_lower, new_higher), view=view)
+
     porc_low = 0.5
     porc_high = 1.5
     iterator = 1
-    
     lower = round(base * porc_low, 2)
     higher = round(base * porc_high, 2)
 
-    embed = MyEmbed(name, icon, iterator, base, lower, higher)
+    lower_button = Button(label = "Lower", style = discord.ButtonStyle.secondary)
+    lower_button.callback = lower_callback
 
-    await ctx.send(embed=embed)
+    higher_button = Button(label = "Higher", style = discord.ButtonStyle.secondary)
+    higher_button.callback = higher_callback
+
+    view = View()
+    view.add_item(lower_button)
+    view.add_item(higher_button)
+
+    message = await ctx.send(embed=MyEmbed(name, icon, iterator, base, lower, higher), view=view)
 
 client.run(token_bot)
